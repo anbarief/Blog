208b2c78760d2a6aee573345c08a7c0197b4cee1
Arief Anbiya <35247692+anbarief@users.noreply.github.com>
2019-02-13 13:54:16 +0700

    Update JediOrc.py

--- Tutorial/Using Python Class for Simple Jedi-Orc game/JediOrc.py
+++ Tutorial/Using Python Class for Simple Jedi-Orc game/JediOrc.py
@@ -1,83 +1,80 @@
 import turtle
-import time
 
-jedi_gif = "jedi.gif"
-orc_gif = "orc.gif"
-darkorc_gif = "darkorc.gif"
-damaged_gif = "damaged.gif" 
+jedi_gif = "/home/asus/Arief_tempo/images/random/jedi.gif"
+orc_gif = "/home/asus/Arief_tempo/images/random/orc.gif"
+darkorc_gif = "/home/asus/Arief_tempo/images/random/darkorc.gif"
+damaged_gif = "/home/asus/Arief_tempo/images/random/damaged.gif"
 
 turtle.register_shape( jedi_gif )
 turtle.register_shape( orc_gif )
 turtle.register_shape( darkorc_gif )
 turtle.register_shape( damaged_gif )
 
+
 class JediLuke:
     def __init__(self):
         self.power = 200
         self.health = 200
-
         self.img = turtle.Turtle( shape = jedi_gif )
-        self.damaged_img = turtle.Turtle( shape = damaged_gif, visible = False )
-    
+        self.damaged_img = turtle.Turtle( shape = damaged_gif )
+        self.damaged_img.hideturtle()
+
         self.img.penup()
         self.damaged_img.penup()
 
     def lightsaber_attack(self, enemy):
+        current_pos = self.img.pos()
+        
         self.img.setpos(enemy.img.pos()[0], enemy.img.pos()[1])
         enemy.damaged_img.showturtle()
-        enemy.health += - self.power
-        time.sleep(1)
+        enemy.health = enemy.health - self.power
 
-        enemy.damaged_img.hideturtle()
         if enemy.health < 0:
             enemy.img.hideturtle()
+            enemy.damaged_img.hideturtle()
         
-        self.img.setpos(200, 0)
+        self.img.setpos(current_pos[0], current_pos[1])
+        enemy.damaged_img.hideturtle()
 
-    def change_pos(self, pos):
-        self.img.setpos(pos[0], pos[1])
-        self.damaged_img.setpos(pos[0], pos[1] + 150)
-       
+    def change_pos(self, pos_x, pos_y):
+        self.img.setpos(pos_x, pos_y)
+        self.damaged_img.setpos(pos_x, pos_y)
+
 
 class Orc:
-    def __init__(self, health, gif_image):
-        self.power = 100
-        self.health = health
+    def __init__(self, orcshape_gif):
+        self.power = 200
+        self.health = 200
+        self.img = turtle.Turtle( shape = orcshape_gif )
+        self.damaged_img = turtle.Turtle( shape = damaged_gif )
+        self.damaged_img.hideturtle()
 
-        self.img = turtle.Turtle( shape = gif_image )
-        self.damaged_img = turtle.Turtle( shape = damaged_gif, visible = False )
-    
         self.img.penup()
         self.damaged_img.penup()
 
     def attack(self, enemy):
         current_pos = self.img.pos()
+        
         self.img.setpos(enemy.img.pos()[0], enemy.img.pos()[1])
         enemy.damaged_img.showturtle()
-        enemy.health += - self.power
-        
-        time.sleep(1)
 
-        enemy.damaged_img.hideturtle()
+        enemy.health = enemy.health - self.power
+
         if enemy.health < 0:
             enemy.img.hideturtle()
-
-        self.img.setpos(current_pos[0], current_pos[1])
-
-    def change_pos(self, pos):
-        self.img.setpos(pos[0], pos[1])
-        self.damaged_img.setpos(pos[0], pos[1] + 150)
-
-luke = JediLuke()
-luke.change_pos( [200, 0] )
+            enemy.damaged_img.hideturtle()
         
-orc_1 = Orc( health = 400 , gif_image = orc_gif)
-orc_1.change_pos( [-200, 100] )
+        self.img.setpos(current_pos[0], current_pos[1])
+        enemy.damaged_img.hideturtle()
 
-orc_2 = Orc( health = 200, gif_image = darkorc_gif )
-orc_2.change_pos( [-200, -100] )
+    def change_pos(self, pos_x, pos_y):
+        self.img.setpos(pos_x, pos_y)
+        self.damaged_img.setpos(pos_x, pos_y)        
 
-#Simulation:
-# - luke.attack( orc_ 1 )
-# - luke.attack( orc_ 2 )
-# - orc_2.attack( luke )
+## SIMULATION:      
+luke = JediLuke()
+redorc = Orc( orc_gif )
+darkorc = Orc( darkorc_gif )
+luke.change_pos( 200, 0 )
+redorc.change_pos( -200, 100 )
+darkorc.change_pos( -200, -100 )
